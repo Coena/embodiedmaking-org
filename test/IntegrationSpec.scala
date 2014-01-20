4c096a3397930aa7db79bc7afd9b62e03954aca8
Arunadevi <aruna@coena.com>
2014-01-20 16:41:25 +0530

    Fix tests

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -4,7 +4,7 @@ import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
-
+import play.api.libs.ws.WS
 /**
  * add your integration spec here.
  * An integration test will fire up a whole play application in a real (or headless) browser
@@ -13,16 +13,18 @@ class IntegrationSpec extends Specification {
   
   "Application" should {
     
-    "work from within a browser" in {
-      running(TestServer(3333), HTMLUNIT) { browser =>
+    /*"work from within a browser" in {
+      running(TestServer(3344), HTMLUNIT) { browser =>
 
-        browser.goTo("http://localhost:3333/")
+        browser.goTo("http://localhost:3344")
 
-        browser.pageSource must contain("Your new application is ready.")
+        browser.pageSource must contain("Embodied")
        
       }
-    }
-    
-  }
+    }*/
+    "run in a server" in new WithServer {
+		  await(WS.url("http://localhost:" + port).get).status must equalTo(OK)
+		}
+	}
   
 }
