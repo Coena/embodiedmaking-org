c37b31ab90343424d028ec9addc5e308a76e6bf8
Daniel Codrea <daniel@product-foundry.com>
2014-12-09 17:10:47 +0200

    Separate page urls

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -50,54 +50,10 @@ $(document).ready(function(){
   };
   $('#main-nav a').click(scrollToPosition);
   
-  var originalAllModalsContent = $('#allModalContent').html();
-  
-  var clickedModalNavigationItem = false;
-  function loadDifferentContent() {
-  $('.load-different-content').each(function () {
-      $(this).on('click', function (e) {
-          e.preventDefault();
-          //get to be loaded div id
-          var toBeLoadedId = $(this).attr('data-content');
-          console.log('to be loaded id = ' + toBeLoadedId);
-          //copy to be loaded content
-          var contentToBeLoaded = $(toBeLoadedId).find('.modal-content').html();
-          //get the opened modal
-          var openModal = $(this).parents('.basics-modal');
-          //get the open modal id
-          var openModalId = $(openModal).attr('id');
-          console.log('open modal id = ' + openModalId);
-          //store original content
-          var originalData = $(openModal).html();
-          if(!clickedModalNavigationItem){
-            $('#basicsModalHolder').html(originalData);
-            clickedModalNavigationItem = true;
-          }
-          //clear old data
-          $(openModal).removeData('bs.modal');
-          //verify if data needed is new or the original
-          if(toBeLoadedId === '#' + openModalId){
-            var originalDataContent = $('#basicsModalHolder').find('.modal-content').html();
-            //append original data it var request data-content is equal to the original modal id
-            $(openModal).find('.modal-content').html(originalDataContent);
-          } else {
-            //append new data
-            $(openModal).find('.modal-content').html(contentToBeLoaded);
-          }
-          $(openModal).on('hide.bs.modal', function () {
-            //reload original modals content on modal close
-            console.log('test');
-            $('#allModalContent').html(originalAllModalsContent);
-            loadDifferentContent();
-          });
-          //show new data
-          $(openModal).modal('show');
-          //make generic nav function available to new content
-          loadDifferentContent();
-        });
-    });
-  }
-  loadDifferentContent();
+  $('.close-modal').click(function(){
+     window.location=$(this).find('a').attr('href');
+     return false;
+  });
   
 });
 
