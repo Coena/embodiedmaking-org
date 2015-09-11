3a484ac6b4b366cc4d793535789ea7efd1211f31
Ni Tran <hung@product-foundry.com>
2015-09-11 16:06:51 +0200

    Included library controller

---
+++ app/scripts/controllers/library.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name embodiedmakingOrgApp.controller:LibraryCtrl
+ * @description
+ * # LibraryCtrl
+ * Controller of the embodiedmakingOrgApp
+ */
+angular.module('embodiedmakingOrgApp')
+  .controller('LibraryCtrl', function ($scope, $resource) {
+  });
