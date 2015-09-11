945d6d0a62602fd5cc8d4bd7292b25e7190db25a
Ni Tran <hung@product-foundry.com>
2015-09-11 16:06:25 +0200

    Minor styling fixes on the essence page

---
+++ app/scripts/controllers/the_essence.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name embodiedmakingOrgApp.controller:TheEssenceCtrl
+ * @description
+ * # TheEssenceCtrl
+ * Controller of the embodiedmakingOrgApp
+ */
+angular.module('embodiedmakingOrgApp')
+  .controller('TheEssenceCtrl', function ($scope, $resource) {
+
+  });
